11796b3e29a96856bda291caeba26770fd667f05
AveryHou1123 <avery.hou@gmail.com>
2017-06-19 22:00:43 +0800

    add Math support

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,14 @@
+/*
 
+curl -X POST \
+  -H "X-Parse-Application-Id: kP6KipEEmdj9w5aZLt6r" \
+  -H "Content-Type: application/json" \
+  -d '{}' \
+  https://donate2navi.herokuapp.com/parse/functions/hello
+  
+  */
 
+require("./Math.uuid.js");
 
 Parse.Cloud.define('hello', function(req, res) {
 	res.success(Math.uuid(10));
