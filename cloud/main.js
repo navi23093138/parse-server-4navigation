54ed4bcd31446784a27bff2b9899c337c5d1b50f
AveryHou1123 <avery.hou@gmail.com>
2017-06-20 15:48:55 +0800

    update

--- cloud/main.js
+++ cloud/main.js
@@ -6,7 +6,7 @@ curl -X POST \
   -d '{
   	"donateType": "monthly",
   	"donateMoney": "1130",
-  	"receiptTitle" : "金城武",
+  	"receiptTitle" : "金城武11",
   	"receiptAddress" : "新竹縣竹北光明一路100",
   	"email" : "avery.hou@gmail.com",
   	"cardNo" : "0001122334401238",
