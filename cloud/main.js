07aca00e41ed7637c0cc88a1efcc2e5e576ca2f3
AveryHou1123 <avery.hou@gmail.com>
2017-06-21 21:10:35 +0800

    update

--- cloud/main.js
+++ cloud/main.js
@@ -6,14 +6,14 @@ curl -X POST \
   -d '{
   	"donateType": "monthly",
   	"donateMoney": "1130",
-  	"receiptTitle" : "金城武11",
+  	"receiptTitle" : "金城武",
   	"receiptAddress" : "新竹縣竹北光明一路100",
   	"email" : "avery.hou@gmail.com",
   	"cardNo" : "0001122334401238",
   	"cardNoYY" : "04",
   	"cardNoMM" : "20",
   	"cardNoCVC" : "987",
-  	"cellPhone" : "0919000000",
+  	"cellPhone" : "0919188224",
   	"homePhone" : "02-23093138"
   
   }' \
@@ -93,39 +93,88 @@ Parse.Cloud.define("findApplicationByPhone", function(request, response) {
 Parse.Cloud.afterSave("NV_DonationApply", function(request) {
 	
 	if (request.object.get("email") != null) {
+		if( request.object.get("status") == "applying") {
 		
-		var donateType = request.object.get("donateType");
-		if (donateType == "once") 
+			var donateType = request.object.get("donateType");
+			if (donateType == "once") 
+			
+			var mailSubj = "已收到您的捐款單，我們會儘速與您聯絡，謝謝";
+			var body = request.object.get("receiptTitle") + " 您好,<BR><BR>";
+			
+			body += "感謝您對領航協會的支持，您的捐款申請單已收到，我們的專員會儘速與您聯絡。<BR><BR>";
+			
+			body += "以下是您的捐款申請資料 -<BR>";
+			
+			body += "捐款方式: <font color='red'>" + ((donateType == "once")? "單次捐款" : "每月定期") + "</font><BR>";
+			body += "捐款金額: <font color='red'>$" + request.object.get("donateMoney") + "</font><BR>";
+			
+			body += "手機: " + request.object.get("cellPhone") + "<BR>";
+			body += "市話: " + request.object.get("homePhone") + "<BR>";
+			body += "收據寄送地址: " + request.object.get("receiptAddress") + "<BR>";
+			
+			body += "信用卡: " + "************" + request.object.get("cardNo").substr(12,16) + "<BR><BR>";
+			
+			
+			body += "領航協會全體人員感謝您的愛心，祝您順心!<BR><BR>";
+			body += "領航協會聯絡方式:<BR>";
+			body += "電話:02-23093138<BR>";
+			
+			logger.send_notify(request.object.get("email"), "", "已收到您的捐款單，我們會儘速與您聯絡，謝謝", body);
+			
+			
+			var applicationInfo = "您可以透過下面的連結查看申請資料處理狀態 :<BR><BR>";
+			applicationInfo += "http://donate.navi.love/application.html?accessToken=" + request.object.id + request.object.get("accessToken");
+			
+			logger.send_notify("navi546@navi.love", "", "有一筆新的捐款單，捐款人:" + request.object.get("receiptTitle") , applicationInfo);
+			return true;
+			
+		} else if( request.object.get("status") == "contacted") {
+			var body = request.object.get("receiptTitle") + " 您好,<BR><BR>";
+			
+			body += "感謝您撥空與我們做資料確認，捐款金額確定扣款後會再通知您<BR><BR>";
+			
+			body += "以下是您的捐款申請資料 -<BR>";
+			
+			body += "捐款方式: <font color='red'>" + ((donateType == "once")? "單次捐款" : "每月定期") + "</font><BR>";
+			body += "捐款金額: <font color='red'>$" + request.object.get("donateMoney") + "</font><BR>";
+			body += "信用卡: " + "************" + request.object.get("cardNo").substr(12,16) + "<BR><BR>";
+			
+			
+			body += "領航協會全體人員感謝您的愛心，祝您順心!<BR><BR>";
+			body += "領航協會聯絡方式:<BR>";
+			body += "電話:02-23093138<BR>";
+			
+			logger.send_notify(request.object.get("email"), "", "感謝您撥空與我們做資料確認", body);
+			
+			
+			var applicationInfo = "您可以透過下面的連結查看申請資料<BR><BR>";
+			applicationInfo += "http://donate.navi.love/application.html?accessToken=" + request.object.id + request.object.get("accessToken");
+			
+			return true;
 		
-		var mailSubj = "已收到您的捐款單，我們會儘速與您聯絡，謝謝";
-		var body = request.object.get("receiptTitle") + " 您好,<BR><BR>";
-		
-		body += "感謝您對領航協會的支持，您的捐款申請單已收到，我們的專員會儘速與您聯絡。<BR><BR>";
-		
-		body += "以下是您的捐款申請資料 -<BR>";
-		
-		body += "捐款方式: <font color='red'>" + ((donateType == "once")? "單次捐款" : "每月定期") + "</font><BR>";
-		body += "捐款金額: <font color='red'>$" + request.object.get("donateMoney") + "</font><BR>";
-		
-		body += "手機: " + request.object.get("cellPhone") + "<BR>";
-		body += "市話: " + request.object.get("homePhone") + "<BR>";
-		body += "收據寄送地址: " + request.object.get("receiptAddress") + "<BR>";
-		
-		body += "信用卡: " + "************" + request.object.get("cardNo").substr(12,16) + "<BR><BR>";
-		
-		
-		body += "領航協會全體人員感謝您的愛心，祝您順心!<BR><BR>";
-		body += "領航協會聯絡方式:<BR>";
-		body += "電話:02-23093138<BR>";
-		
-		logger.send_notify(request.object.get("email"), "", "已收到您的捐款單，我們會儘速與您聯絡，謝謝", body);
-		
-		
-		var applicationInfo = "您可以透過下面的連結查看申請資料<BR><BR>";
-		applicationInfo += "http://donate.navi.love/application.html?accessToken=" + request.object.id + request.object.get("accessToken");
-		
-		logger.send_notify(prop.admin_mail(), "", "有一筆新的捐款單，捐款人:" + request.object.get("receiptTitle") , applicationInfo);
-		return true;
+		} else if( request.object.get("status") == "paid") {
+			var body = request.object.get("receiptTitle") + " 您好,<BR><BR>";
+			
+			body += "感謝您，捐款金額已確定扣款<BR><BR>";
+			
+			body += "以下是您的捐款申請資料 -<BR>";
+			
+			body += "捐款方式: <font color='red'>" + ((donateType == "once")? "單次捐款" : "每月定期") + "</font><BR>";
+			body += "捐款金額: <font color='red'>$" + request.object.get("donateMoney") + "</font><BR>";
+			body += "信用卡: " + "************" + request.object.get("cardNo").substr(12,16) + "<BR><BR>";
+			
+			
+			body += "領航協會全體人員感謝您的愛心，祝您順心!<BR><BR>";
+			body += "領航協會聯絡方式:<BR>";
+			body += "電話:02-23093138<BR>";
+			
+			logger.send_notify(request.object.get("email"), "", "感謝您，捐款金額已確定扣款", body);
+			
+			
+			var applicationInfo = "您可以透過下面的連結查看申請資料<BR><BR>";
+			applicationInfo += "http://donate.navi.love/application.html?accessToken=" + request.object.id + request.object.get("accessToken");
+				
+		}
 	}
 });
 
